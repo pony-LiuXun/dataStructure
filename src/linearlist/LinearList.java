af39c10d441918977c920f1550ca25d045dc9675
liuxun <liuxun030792>
2019-02-26 18:57:38 +0800

    线性表

---
+++ src/linearlist/LinearList.java
@@ -0,0 +1,66 @@
+package linearlist;
+
+/**
+ * <p>线性表的抽象数据类型(基本API操作)</p>
+ *
+ * @author pony-liuxun
+ * @date 2019/02/26
+ */
+public interface LinearList {
+    /**
+     * <p>清空线性表</p>
+     */
+    void clear();
+
+    /**
+     * <p>判断线性表是否为空</p>
+     *
+     * @return <p>空返回true,否则返回false</p>
+     */
+    boolean isEmpty();
+
+    /**
+     * <p>获取线性表的元素个数</p>
+     *
+     * @return <p>返回线性表的元素个数</p>
+     */
+    int length();
+
+    /**
+     * <p>获取线性表的中某个元素</p>
+     *
+     * @param index 线性变元素下标
+     * @return <p>返回对应的下标元素</p>
+     */
+    Object get(int index);
+
+    /**
+     * <p>在线性表中的某个位置插入元素</p>
+     *
+     * @param index 插入位置
+     * @param b     插入的元素
+     */
+    void insert(int index, Object b);
+
+    /**
+     * <p>删除指定位置的元素</p>
+     *
+     * @param index 元素下标
+     */
+    void remove(int index);
+
+    /**
+     * <p>输出表中的内容</p>
+     */
+    void display();
+
+    /**
+     * <p>判断该元素是否存在于表中，若存在返回元素下标,不存在返回-1</p>
+     *
+     * @param b 验证元素
+     * @return
+     */
+    int indexOf(Object b);
+
+
+}
---
+++ src/linearlist/sequencelist/SequenceList.java
@@ -0,0 +1,61 @@
+package linearlist.sequencelist;
+
+import linearlist.LinearList;
+
+/**
+ * <p>线性表☞顺序表(java-实现),此实现只是粗糙的实现，可自行完善</p>
+ * @author pony-liuxun
+ * @date 2019/02/26
+ */
+public class SequenceList implements LinearList {
+    /**
+     * 线性表的存储空间
+     */
+    private Object [] listElem;
+
+    /**
+     * 线性表的当前长度
+     */
+    private int length;
+
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public void insert(int index, Object b) {
+
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public int indexOf(Object b) {
+        return 0;
+    }
+}
