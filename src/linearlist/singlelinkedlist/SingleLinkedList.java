201580d487ba1bb83c9b203ef53682b8e9883abc
liuxun <liuxun030792>
2019-02-28 18:00:18 +0800

    线性表--单链表实现一

---
+++ src/linearlist/singlelinkedlist/SingleLinkedList.java
@@ -0,0 +1,49 @@
+package linearlist.singlelinkedlist;
+
+import linearlist.LinearList;
+
+/**
+ * <p>线性表☞单链表(java-实现),此实现只是粗糙的实现，可自行完善</p>
+ */
+public class SingleLinkedList implements LinearList {
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int length() {
+        return 0;
+    }
+
+    @Override
+    public Object get(int index) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void insert(int index, Object b) throws Exception {
+
+    }
+
+    @Override
+    public void remove(int index) throws Exception {
+
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public int indexOf(Object b) {
+        return 0;
+    }
+}
